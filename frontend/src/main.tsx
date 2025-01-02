3853ca5d5a88d333c82059f651ad8992c0c2921c
jpacas <josepacas@gmail.com>
2025-01-02 04:22:31 -0600

    Corrigiendo importacion del tema de material ui

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,7 +5,7 @@ import App from './App.tsx'
 import { AuthProvider } from './context/AuthContext.tsx'
 import { ThemeProvider } from '@mui/material/styles'
 import CssBaseline from '@mui/material/CssBaseline'
-import theme from './theme/Theme.ts'
+import theme from '../src/theme/theme.ts'
 
 createRoot(document.getElementById('root')!).render(
   <AuthProvider>
