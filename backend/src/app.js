6f3c0fa733a9c20791f809aadc715ce872b41f89
jpacas <josepacas@gmail.com>
2025-02-10 10:04:27 -0600

    Agregando dominio adicional al cors para pruebas

--- backend/src/app.js
+++ backend/src/app.js
@@ -15,7 +15,11 @@ dotenv.config()
 const app = express()
 app.use(
   cors({
-    origin: ['https://zucarlink.com', 'http://localhost:5173'], // Lista de orígenes permitidos
+    origin: [
+      'https://zucarlink.com',
+      'http://localhost:5173',
+      'https://zucarlink-git-pruebas-jose-pacas-projects.vercel.app',
+    ], // Lista de orígenes permitidos
     methods: ['GET', 'POST', 'PUT', 'DELETE'], // Métodos HTTP permitidos
     allowedHeaders: ['Content-Type', 'Authorization'], // Cabeceras permitidas
   })
